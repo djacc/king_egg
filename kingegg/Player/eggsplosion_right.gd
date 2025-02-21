b317bf8358a0ea908f2636bfaa2449d4e265b937
jarlsoeren <jarlsoeren@gmail.com>
2025-02-21 16:10:10 +0100

    egg break

---
+++ kingegg/Player/eggsplosion_right.gd
@@ -0,0 +1,7 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	linear_velocity = Vector2(randf_range(0, 500), randf_range(-200, -500))
+	angular_velocity = randf_range(10, 20)
