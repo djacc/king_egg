be953e88863e99b1c50ff228b0d841942b657380
jarlsoeren <jarlsoeren@gmail.com>
2025-02-23 11:16:50 +0100

    lol

--- kingegg/MainScene/system.gd
+++ kingegg/MainScene/system.gd
@@ -5,11 +5,11 @@ var current_level_number: int = 1
 @onready var story_button = $"../ClickSound"
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("escape"):
-		gamecloser.emit()
-		await get_tree().create_timer(0.05).timeout
-		get_tree().quit()
-	elif Input.is_action_just_pressed("respawn"):
+	#if Input.is_action_just_pressed("escape"):
+		#gamecloser.emit()
+		#await get_tree().create_timer(0.05).timeout
+		#get_tree().quit()
+	if Input.is_action_just_pressed("respawn"):
 		#get_tree().reload_current_scene()
 		print(current_level_number)
 		reload_current_level()
